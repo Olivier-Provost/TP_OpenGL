ac2e7489516471f5e05f0a4ce64d56181600f606
Olivier <>
2018-01-09 08:30:19 -0500

    Le vrai projet

--- main.cpp
+++ main.cpp
@@ -29,6 +29,46 @@ SDL_GLContext ctx;
 GLuint idBrick;
 bool isRunning = true;
 
+bool checkCollision( SDL_Rect a, SDL_Rect b )
+{
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
+
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;
+
+    if( bottomA <= topB )
+    {
+        return false;
+    }
+
+    if( topA >= bottomB )
+    {
+        return false;
+    }
+
+    if( rightA <= leftB )
+    {
+        return false;
+    }
+
+    if( leftA >= rightB )
+    {
+        return false;
+    }
+
+    return true;
+}
+
 class Coord {
 public:
     int xMin, xMax, yMin, yMax;
